7b535de2ac1134b58787a2400c7d2bce83ca3c50
Robin Meyer <r.meyer@rola.com>
2020-12-17 17:25:26 +0100

    chore: initial commit

---
+++ stencil-app/src/components/app-home/app-home.tsx
@@ -0,0 +1,17 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'app-home',
+  styleUrl: 'app-home.css',
+  shadow: false,
+})
+export class AppHome {
+  render() {
+    return (
+      <div class="app-home">
+        App home.
+        <rs-root/>
+      </div>
+    );
+  }
+}
